08bf14c8cc7946460b3db54470c4a00087669252
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-06-06 16:46:25 -0400

    users table methods

--- app/utl/databasing.py
+++ app/utl/databasing.py
@@ -25,6 +25,7 @@ def init_tables():
             ] ,
         'users' : [
             ['uid','INTEGER PRIMARY KEY'],
+            ['email','TEXT'],
             ['username','TEXT'],
             ['access_token','TEXT']
             ] ,
@@ -53,5 +54,43 @@ def init_tables():
     db.commit()
     return True
 
+# =============== USERS DATABASE ===============
+
+def update_user(uid,username,email,token):
+    """Either add a new user to the database, or updates the existing entry for them.
+    Returns boolean `fresh`, True if user is added for the first time, False otherwise
+    """
+    c.execute(
+        'INSERT OR REPLACE INTO users (uid,username,email,access_token) VALUES (?,?,?,?);',
+        (int(uid),username,email,token)
+        )
+    db.commit()
+    pass
+
+def get_token(uid):
+    """Returns current access token for a specified uid"""
+    c.execute('SELECT access_token FROM users WHERE uid=?;',(int(uid),))
+    db.commit()
+    res = c.fetchone()
+    if res:
+        return res[0]
+    raise NonexistentUserException('uid not found')
+
+def get_userinfo(uid):
+    """Returns {username, email} for a specified uid"""
+    c.execute('SELECT username,email FROM users WHERE uid=?;',(int(uid),))
+    db.commit()
+    res = c.fetchone()
+    if res:
+        return {'username':res[0],'email':res[1]}
+    raise NonexistentUserException('uid not found')
+
 # =============== STARTUP FUNCTION CALLS ===============
 init_tables()
+
+
+# =============== TEST CODE ===============
+# update_user(3000000001,'bobama','bobama@gmail.com','eifadkmaskmdfdxc')
+# update_user('58689492321','bobama','barack@gmail.com','jlfkeskdldfj')
+# print(get_token('3000000001'))
+# print(get_userinfo(58689492321))

e98ae955b75aeba878427d6f3e3499adc6019c59
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-06-06 18:34:51 -0400

    fix variety of sqlite bugs

--- app/utl/databasing.py
+++ app/utl/databasing.py
@@ -8,10 +8,7 @@ import os
 DIR = os.path.dirname(__file__) or '.'
 DIR += '/'
 
-db = sqlite3.connect(DIR+'../saucer.db')
-c = db.cursor()
-atexit.register(lambda: db.close()) # idk if this is necessary
-
+DB_FILENAME=DIR+'../saucer.db'
 
 def init_tables():
     tables = {
@@ -24,14 +21,14 @@ def init_tables():
             ['rating_count','INTEGER']
             ] ,
         'users' : [
-            ['uid','INTEGER PRIMARY KEY'],
+            ['uid','TEXT UNIQUE'], # its always gonna be a really big number but-LEAVE IT AS A STRING!
             ['email','TEXT'],
             ['username','TEXT'],
             ['access_token','TEXT']
             ] ,
         'bookshelves' : [
             ['shelf_id','INTEGER PRIMARY KEY'],
-            ['uid','INTEGER'],
+            ['uid','TEXT'],
             ['title','TEXT'],
             ['description','TEXT']
             ] ,
@@ -48,42 +45,51 @@ def init_tables():
             ['shelf_id','INTEGER']
             ]
         }
+    db = sqlite3.connect(DB_FILENAME)
+    c = db.cursor()
     for table in tables:
         command = 'CREATE TABLE IF NOT EXISTS %s (%s)' % (table,', '.join(map(lambda col: ' '.join(col),tables[table])) )
         c.execute(command)
     db.commit()
+    db.close()
     return True
 
 # =============== USERS DATABASE ===============
 
 def update_user(uid,username,email,token):
     """Either add a new user to the database, or updates the existing entry for them.
-    Returns boolean `fresh`, True if user is added for the first time, False otherwise
     """
+    db = sqlite3.connect(DB_FILENAME)
+    c = db.cursor()
     c.execute(
         'INSERT OR REPLACE INTO users (uid,username,email,access_token) VALUES (?,?,?,?);',
-        (int(uid),username,email,token)
+        (uid,username,email,token)
         )
     db.commit()
-    pass
+    db.close()
 
 def get_token(uid):
     """Returns current access token for a specified uid"""
-    c.execute('SELECT access_token FROM users WHERE uid=?;',(int(uid),))
-    db.commit()
+    db = sqlite3.connect(DB_FILENAME)
+    c = db.cursor()
+    c.execute('SELECT access_token FROM users WHERE uid=?;',(uid,))
     res = c.fetchone()
+    db.close()
     if res:
         return res[0]
-    raise NonexistentUserException('uid not found')
+    raise KeyError('uid not found')
 
 def get_userinfo(uid):
     """Returns {username, email} for a specified uid"""
-    c.execute('SELECT username,email FROM users WHERE uid=?;',(int(uid),))
+    db = sqlite3.connect(DB_FILENAME)
+    c = db.cursor()
+    c.execute('SELECT username,email FROM users WHERE uid=?;',(uid,))
     db.commit()
     res = c.fetchone()
+    db.close()
     if res:
         return {'username':res[0],'email':res[1]}
-    raise NonexistentUserException('uid not found')
+    raise KeyError('uid not found')
 
 # =============== STARTUP FUNCTION CALLS ===============
 init_tables()

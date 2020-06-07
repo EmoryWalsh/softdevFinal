6aae7b41e2bae5a464a72ce52ce9b630f8756c10
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-06-07 14:59:01 -0400

    restore login code

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+# Flying Saucers
+# Softdev2 pd9
+# P05 -- The Bookshelf / Flask Routes
+# 2020-06-05
+
+from flask import Flask, render_template, request, session, url_for, redirect, flash
+import os
+
+from utl import databasing as db, google
+
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
 
 @app.route('/')
 def home():
@@ -17,6 +28,32 @@ def bookfinder():
 @app.route('/help')
 def help():
     return render_template("help.html")
+
+@app.route('/auth')
+def google_auth():
+    """Handle Google redirects, callbacks, etc."""
+    if 'error' in request.args:
+        # failure to authenticate
+        print('error in request.args')
+        print(request.url)
+        return redirect( url_for('home') )
+    if 'code' in request.args:
+        # then user has already authorized: need to review scopes, exchange for tokens
+        # TODO @kiran: review scopes!
+        # print(request.args['code'])
+        session['uid'] = google.trade_for_tokens( request.args['code'], request.base_url )
+        userinfo = db.get_userinfo(session['uid'])
+        flash('You have been logged in. Welcome, {0[username]}!'.format(userinfo))
+        return redirect( url_for('home') )
+    else:
+        # user has just clicked a "login" link, needs to be redirected to google
+        if 'uid' in session:
+            flash('You are already logged in!')
+            return redirect( url_for('home') )
+        auth_url = google.gen_authlink( request.base_url )
+        # print(auth_url)
+        return redirect( auth_url )
+
 
 if __name__ == '__main__':
     app.debug = True

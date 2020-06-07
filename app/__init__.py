55913311aee9a33102ce019e211915df08737ca0
Emory Walsh <ewalsh00@stuy.edu>
2020-06-06 21:01:16 -0400

    Added basic template

--- app/__init__.py
+++ app/__init__.py
@@ -1,46 +1,22 @@
-# Flying Saucers
-# Softdev2 pd9
-# P05 -- The Bookshelf / Flask Routes
-# 2020-06-05
-
-from flask import Flask, render_template, url_for, request, redirect, session, flash
-
-import utl.databasing as db
-from utl import google
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.secret_key = os.urandom(32)
-
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/auth')
-def google_auth():
-    """Handle Google redirects, callbacks, etc."""
-    if 'error' in request.args:
-        # failure to authenticate
-        print('error in request.args')
-        print(request.url)
-        return redirect( url_for('home') )
-    if 'code' in request.args:
-        # then user has already authorized: need to review scopes, exchange for tokens
-        # TODO @kiran: review scopes!
-        # print(request.args['code'])
-        session['uid'] = google.trade_for_tokens( request.args['code'], request.base_url )
-        userinfo = db.get_userinfo(session['uid'])
-        flash('You have been logged in. Welcome, {0[username]}!'.format(userinfo))
-        return redirect( url_for('home') )
-    else:
-        # user has just clicked a "login" link, needs to be redirected to google
-        if 'uid' in session:
-            flash('You are already logged in!')
-            return redirect( url_for('home') )
-        auth_url = google.gen_authlink( request.base_url )
-        # print(auth_url)
-        return redirect( auth_url )
+@app.route('/myshelves')
+def myshelves():
+    return render_template("myshelves.html")
+
+@app.route('/bookfinder')
+def bookfinder():
+    return render_template("bookfinder.html")
+
+@app.route('/help')
+def help():
+    return render_template("help.html")
 
 if __name__ == '__main__':
     app.debug = True
